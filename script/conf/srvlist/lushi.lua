924ce5cd5fd287dd6cf626135727e7c8b4c2c3ba
sundream <linguanglianglgl@gmail.com>
2016-03-19 18:56:34 +0800

    游戏服监听端口修改

--- script/conf/srvlist/lushi.lua
+++ script/conf/srvlist/lushi.lua
@@ -2,14 +2,14 @@ return {
 	gamesrv_100 = {
 		name = "gamesrv_100",
 		ip = "127.0.0.1",
-		port = 8000,
+		port = 8001,
 		minroleid = 10000,
 		maxroleid = 1000000,
 	},
 	gamesrv_101 = {
 		name = "gamesrv_101",
 		ip = "127.0.0.1",
-		port = 8001,
+		port = 8002,
 		minroleid = 1000000,
 		maxroleid = 2000000,
 	},
