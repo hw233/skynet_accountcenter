e45eae6cddc4bcff0cf3505e113ed8e150401651
sundream <linguanglianglgl@gmail.com>
2016-05-27 17:26:46 +0800

    与帐号中心http通信回复格式统一由:errcode|result改成{errcode=errcode,result=result}

--- script/game.lua
+++ script/game.lua
@@ -8,33 +8,47 @@ require "script.hotfix.init"
 require "script.gm.init"
 require "script.banlogin"
 
+local function _print(...)
+	print(...)
+	skynet.error(...)
+end
+
 game = game or {}
 
 function game.startgame()
 	print("Startgame...")
 	console.init()
+	_print("console.init")
 	logger.init()
+	_print("logger.init")
 	dbmgr.init()
+	_print("dbmgr.init")
 	acctmgr.init()
+	_print("acctmgr.init")
 	gm.init()
+	_print("gm.init")
 	oscmd.init()
+	_print("oscmd.init")
 	game.initall = true
-	print("Startgame ok")
+	_print("Startgame ok")
 	logger.log("info","game","[startgame]")
 end
 
 function game.shutdown(reason)
 	game.initall = nil
-	print("Shutdown")
+	_print("Shutdown")
 	logger.log("info","game",string.format("[shutdown start] reason=%s",reason))
 	dbmgr.shutdown()
+	_print("dbmgr.shutdown")
 	for i,agent in ipairs(__agents) do
 		skynet.send(agent,"lua","exec","dbmgr.shutdown()")
 	end
 	timer.timeout("timer.shutdown",20,function ()
+		_print("logger.shutdown")
 
 		logger.log("info","game",string.format("[shutdown success] reason=%s",reason))
 		logger.shutdown()
+		_print("Shutdown ok")
 		os.execute(string.format("cd ../shell/ && sh killserver.sh %s",skynet.getenv("srvname")))
 	end)
 end
